4c4f75af3aa27deb8e07acfdb124e3ae9c249ee1
hysunli <1209241788@qq.com>
2017-07-04 21:57:48 +0800

    1

---
+++ Base.php
@@ -0,0 +1,34 @@
+<?php
+namespace houdunwang\view;
+class Base{
+	private $file;
+	private $var = [];
+
+	/**
+	 * 载入模板
+	 */
+	public function make(){
+		//include "../app/home/view/entry/index.php";
+		$this->file = "../app/". MODULE . "/view/" . CONTROLLER . "/".ACTION.".php";
+		return $this;
+	}
+
+	public function with($data){
+
+		$this->var = $data;
+		return $this;
+	}
+
+	/**
+	 * 当输出对象，会自动触发此方法
+	 * @return string
+	 */
+	public function __toString() {
+		//把键名变为变量名，键值变为变量值
+		extract($this->var);//相当于$data = 'lumiya';
+		include $this->file;
+		return '';
+	}
+
+
+}
---
+++ View.php
@@ -0,0 +1,11 @@
+<?php
+namespace houdunwang\view;
+class View{
+	public static function __callStatic( $name, $arguments ) {
+		return call_user_func_array([new Base,$name],$arguments);
+	}
+
+	public function __call( $name, $arguments ) {
+		return call_user_func_array([new Base,$name],$arguments);
+	}
+}
